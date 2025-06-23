8360056be08095dfccfe5403f4de8ecbe5f5335b
mahmudurmahid <m.mahid@icloud.com>
2025-06-23 13:44:52 +0100

    notes for asynchronous js async/await

--- script3.js
+++ script3.js
@@ -1,9 +1,19 @@
 /* L: ASYNCHRONOUS JS - PROMISES */
 // PROMISES EXAMPLE
-console.log(fetch("https://jsonplaceholder.typicode.com/todos/"));
+// console.log(fetch("https://jsonplaceholder.typicode.com/todos/"));
 
-fetch("https://jsonplaceholder.typicode.com/todos/")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log("jonas");
+// console.log("jonas");
+
+/* M: ASYNCHRONOUS JS - ASYNC/AWAIT */
+// ASYNC/AWAIT EXAMPLE
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await res.json();
+  console.log(data);
+}
+
+getTodos();
